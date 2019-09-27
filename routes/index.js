7c5fd252bfe8649d2e921286bcdd10a1d040b0d7
ben-tilden <ben-tilden@gmail.com>
2019-09-27 12:49:30 -0400

    Added message serving functionality.
    Added sendMessage and updateMessages to smsClient
    Updated index.js and index.ejs to serve messages on GET request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const accountSid = 'AC02c3544de9e4cd1c8f4c8bcbac203227';
+const authToken = 'b5c4a0f28c439a59abb20eb0dc0c7e06';
+const twilioClient = require('twilio')(accountSid, authToken);
+
+const smsClient = require('../public/javascripts/smsClient');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Control Center' });
+router.get('/', async (req, res, next) => {
+  const updatedMessages = await smsClient.updateMessages(twilioClient);
+  res.render('index', {
+    title: 'Control Center',
+    messages: updatedMessages
+  });
 });
 
 module.exports = router;
