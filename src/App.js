680abf356d7d26d939dc0a17bfd3b57e1ce740fe
ben-tilden <ben-tilden@gmail.com>
2019-09-30 13:44:55 -0400

    Finished integrating with Node/Express
    Resolved #1

--- src/App.js
+++ src/App.js
@@ -5,29 +5,35 @@ import './App.css';
 class SentMessages extends React.Component {
   constructor(props) {
       super(props);
-      this.state = { messages: [] };
+      this.state = {
+        messages: [],
+        loaded: false
+      }
   }
 
   componentDidMount() {
     fetch('/messages')
       .then(res => res.json())
-      .then(messages => this.setState({ messages }));
+      .then(loadedMessages => this.setState({ messages: loadedMessages, loaded: true }))
+      .catch((err) => this.setState({ loaded: true }));
   }
 
   render() {
-    if (!this.state.messages) {
-      return <p>No Messages to be Found</p>
+    if (this.state.messages.length === 0 && this.state.loaded === false) {
+      return <p>Loading messages...</p>
+    } else if (this.state.messages.length === 0 && this.state.loaded === true) {
+      return <p>No messages to be found.</p>
     } else {
       return (
-        <dl>
+        <div className="messageFeed">
           {this.state.messages.map((message) =>
-            <React.Fragment>
-              <dt key={message.id}>{message.body}</dt>
-              <dd key={message.id}>{message.date}</dd>
-              <dd key={message.id}>{message.rec}</dd>
-            </React.Fragment>
+            <div key={message.id} className="message">
+              <p className="body">{message.body}</p>
+              <p className="date">{message.date}</p>
+              <p className="rec">{message.rec}</p>
+            </div>
           )}
-        </dl>
+        </div>
       )
     }
   }
@@ -37,8 +43,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Mantra Health</h1>
       </header>
-      <h1>Messages</h1>
+      <h2>Messages</h2>
       <SentMessages />
     </div>
   );
