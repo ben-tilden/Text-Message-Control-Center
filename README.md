2cc27ee3006c4912bd720c17e3f3bd5474473e0c
ben-tilden <ben-tilden@gmail.com>
2019-10-01 02:33:16 -0400

    Added message serving functionality
    Resolved #2
    Added proxying for user get requests to serve message contents on page
    Added and implemented react-headroom dependency
    Added and implemented react-infinite-scroller dependency
    Changed create-react-app's logos
    Removed unnecessary files for deployment
    Added Firebase usage

--- README.md
+++ README.md
@@ -1,68 +1,21 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Text Message Control Center Frontend
 
-## Available Scripts
+Text Message Control Center is an application which displays all text messages sent via  a Twilio phone number. It has the functionality to send messages via POST requests to its server. This is the frontend of the application.
 
-In the project directory, you can run:
+Most of the heavy lifting is done in src/App.js
 
-### `npm start`
+## Dependencies
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+See package.json for dependencies
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+## Built With
 
-### `npm test`
+* react: https://www.npmjs.com/package/react
+* react-dom: https://www.npmjs.com/package/react-dom
+* react-scripts: https://www.npmjs.com/package/react-scripts
+* react-headroom: https://www.npmjs.com/package/react-headroom
+* react-infinite-scroller: https://www.npmjs.com/package/react-infinite-scroller
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Author
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Ben Tilden
--- src/App.js
+++ src/App.js
@@ -1,52 +1,118 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import Headroom from "react-headroom";
+import InfiniteScroll from "react-infinite-scroller";
 
+const api = {
+  baseUrl: 'https://api.twilio.com'
+};
+
+// Class for sent messages
 class SentMessages extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        messages: [],
-        loaded: false
+        allMessages: [],
+        nextPage: null,
+        hasMoreItems: true
       }
   }
 
+  // Loads first page of messages
   componentDidMount() {
     fetch('/messages')
       .then(res => res.json())
-      .then(loadedMessages => this.setState({ messages: loadedMessages, loaded: true }))
+      .then(res => this.setState({
+              allMessages: res.messages,
+              nextPage: res.next_page_uri
+            }))
       .catch((err) => this.setState({ loaded: true }));
   }
 
-  render() {
-    if (this.state.messages.length === 0 && this.state.loaded === false) {
-      return <p>Loading messages...</p>
-    } else if (this.state.messages.length === 0 && this.state.loaded === true) {
-      return <p>No messages to be found.</p>
-    } else {
-      return (
-        <div className="messageFeed">
-          {this.state.messages.map((message) =>
-            <div key={message.id} className="message">
-              <p className="body">{message.body}</p>
-              <p className="date">{message.date}</p>
-              <p className="rec">{message.rec}</p>
-            </div>
-          )}
-        </div>
-      )
+  // Load more pages for Infinite Scroll
+  loadNextPage(page) {
+    var self = this;
+    let nextPageUrl = "";
+    if (this.state.nextPage) {
+      nextPageUrl = api.baseUrl + this.state.nextPage;
     }
+    const obj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        url : nextPageUrl
+      })
+    };
+    fetch('/messages', obj)
+      .then(res => res.json())
+      .then(res => {
+        if (res) {
+          var updatedMessages = self.state.allMessages;
+          res.messages.map(message => {
+            updatedMessages.push(message);
+          })
+          if (res.nextPage) {
+            self.setState({
+              allMessages: updatedMessages,
+              nextPage: res.nextPage
+            });
+          } else {
+            self.setState({
+              hasMoreItems: false
+            });
+          }
+        }
+      })
+      .catch((err) => {console.log(err);});
+  }
+
+  render() {
+    const loader = <p className="loader">Loading messages...</p>;
+
+    var items = [];
+    this.state.allMessages.map(message => {
+      items.push(
+        <div key={message.id} className="message">
+        <p className="body">{message.body}</p>
+        <p className="date">{new Date(message.date_sent).toLocaleString()}</p>
+        <p className="rec">{message.to}</p>
+      </div>
+      );
+      return items;
+    });
+
+    return (
+        <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadNextPage.bind(this)}
+            hasMore={this.state.hasMoreItems}
+            loader={loader}>
+
+            <div className="messageFeed">
+                {items}
+            </div>
+        </InfiniteScroll>
+    );
   }
 }
 
+// Return app
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Mantra Health</h1>
-      </header>
-      <h2>Messages</h2>
-      <SentMessages />
+      <Headroom>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Mantra Health</h1>
+        </div>
+      </Headroom>
+      <div>
+        <h2>Messages</h2>
+        <SentMessages />
+      </div>
     </div>
   );
 }
