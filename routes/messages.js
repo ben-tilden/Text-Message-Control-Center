c1121fa802633919f5583571d39d810911081ad8
ben-tilden <ben-tilden@gmail.com>
2019-10-14 14:32:27 -0400

    Revert "Removed and changed authToken"
    This reverts commit ab655a675dd0bbf24455cf33152680cfb2e9be6e.

--- routes/messages.js
+++ routes/messages.js
@@ -4,11 +4,10 @@ const fetch = require('node-fetch');
 const express = require('express');
 const router = express.Router();
 
-const authTokenImport = require('../private/authToken')
 const api = {
   baseUrl: 'https://api.twilio.com',
   accountSid: 'AC02c3544de9e4cd1c8f4c8bcbac203227',
-  authToken: authTokenImport
+  authToken: 'b5c4a0f28c439a59abb20eb0dc0c7e06'
 };
 
 const authHeaders = new fetch.Headers();
